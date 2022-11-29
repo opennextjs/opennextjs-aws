19196c2c86cd980b050951c107ed37954449629c
Jay V <air@live.ca>
2022-11-29 17:40:00 -0500

    Updating bworkflow

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true,
+
   swcMinify: true,
+  reactStrictMode: true,
+
+  basePath: basePath,
+  assetPrefix: assetPrefix,
 }
 
 module.exports = nextConfig
