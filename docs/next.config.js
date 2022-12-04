43fe01fea40b6c3a3ac73d905ca8c23212c8989d
Jay V <air@live.ca>
2022-12-03 21:30:06 -0500

    Removing base path

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,17 +3,17 @@
 let basePath = undefined
 let assetPrefix = undefined
 
-/** Deploys as a directory through GitHub Actions **/
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-/** End GitHub Actions case **/
+///** Deploys as a directory through GitHub Actions **/
+//const isGithubActions = process.env.GITHUB_ACTIONS || false
+//
+//if (isGithubActions) {
+//  // trim off `<owner>/`
+//  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+//
+//  assetPrefix = `/${repo}/`
+//  basePath = `/${repo}`
+//}
+///** End GitHub Actions case **/
 
 const nextConfig = {
   images: {
