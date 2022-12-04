f5b4385392615bb1e6bae2d3c20803703e33f286
Jay V <air@live.ca>
2022-12-04 17:25:06 -0500

    Fixing share and favicon

--- docs/components/Layout.tsx
+++ docs/components/Layout.tsx
@@ -9,8 +9,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <title key="title">{SITE.title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta key="description" name="description" content={SITE.description} />
-        <link rel="icon" href="/open-next/favicon.svg" />
-        <meta property="og:image" content="/open-next/share.png" />
+        <link rel="icon" href="/favicon.svg" />
+        <meta property="og:image" content="/share.png" />
       </Head>
       <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>
