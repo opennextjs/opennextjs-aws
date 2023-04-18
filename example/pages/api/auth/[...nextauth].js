5614a69cd1ed3b49290042d901f4e5e454bed9f1
Frank <wangfanjie@gmail.com>
2023-04-18 06:17:01 -0400

    Example: add NextAuth example

---
+++ example/pages/api/auth/[...nextauth].js
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { Config } from "sst/node/config";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: Config.GITHUB_CLIENT_ID,
+      clientSecret: Config.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
