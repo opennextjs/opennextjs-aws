1d50eb9fba8b3eca6d7a0cf1b5c946d197a22b02
Frank <wangfanjie@gmail.com>
2023-05-17 13:19:08 -0400

    Add ability to warm server function

--- packages/open-next/src/adapters/server-adapter.ts
+++ packages/open-next/src/adapters/server-adapter.ts
@@ -9,11 +9,12 @@ import type {
 } from "aws-lambda";
 // @ts-ignore
 import NextServer from "next/dist/server/next-server.js";
-import { loadConfig, setNodeEnv } from "./util.js";
+import { generateUniqueId, loadConfig, setNodeEnv } from "./util.js";
 import { isBinaryContentType } from "./binary.js";
 import { debug } from "./logger.js";
 import type { PublicFiles } from "../build.js";
 import { convertFrom, convertTo } from "./event-mapper.js";
+import { WarmerEvent, WarmerResponse } from "./warmer-function.js";
 
 setNodeEnv();
 setNextjsServerWorkingDirectory();
@@ -25,7 +26,8 @@ const publicAssets = loadPublicAssets();
 setNextjsPrebundledReact(config);
 debug({ nextDir });
 
-// Create a NextServer
+// Generate a 6 letter unique server ID
+const serverId = `server-${generateUniqueId()}`;
 const requestHandler = new NextServer.default({
   hostname: "localhost",
   port: Number(process.env.PORT) || 3000,
@@ -42,8 +44,17 @@ const requestHandler = new NextServer.default({
 /////////////
 
 export async function handler(
-  event: APIGatewayProxyEventV2 | CloudFrontRequestEvent | APIGatewayProxyEvent
+  event:
+    | APIGatewayProxyEventV2
+    | CloudFrontRequestEvent
+    | APIGatewayProxyEvent
+    | WarmerEvent
 ) {
+  // Handler warmer
+  if ("type" in event) {
+    return formatWarmerResponse(event);
+  }
+
   debug("event", event);
 
   // Parse Lambda event and create Next.js request
@@ -163,4 +174,13 @@ function formatAPIGatewayFailoverResponse() {
 
 function formatCloudFrontFailoverResponse(event: CloudFrontRequestEvent) {
   return event.Records[0].cf.request;
+}
+
+function formatWarmerResponse(event: WarmerEvent) {
+  console.log(event);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ serverId } satisfies WarmerResponse);
+    }, event.delay);
+  });
 }
