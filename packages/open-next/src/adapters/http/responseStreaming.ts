4a2682a265f7db769fb08053f66259edf8bc7093
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2023-08-13 11:52:02 +0200

    perf: use setImmediate instead

--- packages/open-next/src/adapters/http/responseStreaming.ts
+++ packages/open-next/src/adapters/http/responseStreaming.ts
@@ -4,17 +4,13 @@ import { debug, error } from "../logger.js";
 import { getString, headerEnd } from "./utils.js";
 
 const HEADERS = Symbol();
-const BODY = Symbol();
 
 export class StreamingServerResponse extends http.ServerResponse {
   [HEADERS]: Record<string, string> = {};
-  [BODY]: any[] = [];
   responseStream: ResponseStream;
   fixHeaders: (headers: Record<string, string>) => void;
   private _wroteHeader = false;
-  private _done = false;
   private _hasWritten = false;
-  timer: NodeJS.Timer | null = null;
 
   get headers() {
     return this[HEADERS];
@@ -43,8 +39,8 @@ export class StreamingServerResponse extends http.ServerResponse {
     if (this._wroteHeader) {
       return this;
     }
-    this.fixHeaders(this[HEADERS]);
     try {
+      this.fixHeaders(this[HEADERS]);
       this.responseStream = awslambda.HttpResponseStream.from(
         this.responseStream,
         {
@@ -52,9 +48,6 @@ export class StreamingServerResponse extends http.ServerResponse {
           headers: this[HEADERS],
         }
       );
-      if (!this.timer) {
-        this.createListener();
-      }
 
       this._wroteHeader = true;
       debug("writeHead", this[HEADERS]);
@@ -66,32 +59,6 @@ export class StreamingServerResponse extends http.ServerResponse {
     return this;
   }
 
-  // WORKAROUND: It seems that lambda streaming require the event loop to be free
-  //             to start the streaming process. So we need to give it some time
-  // TODO: Find a better way to do this
-  private createListener = () => {
-    this.timer = setInterval(() => {
-      if (this[BODY].length) {
-        const toWrite = getString(this[BODY].shift());
-        debug("write", toWrite);
-        if (this.responseStream.writableNeedDrain) return;
-        this.responseStream.write(toWrite);
-        this._hasWritten = true;
-        return;
-      }
-      if (this._done) {
-        debug("done", this._done);
-        clearInterval(this.timer!);
-        if (!this._hasWritten) {
-          // We need to send data here, otherwise the stream will not end at all
-          this.responseStream.write(new Uint8Array(8));
-        }
-        this.responseStream.end();
-        return;
-      }
-    }, 1);
-  };
-
   end(cb?: (() => void) | undefined): this;
   end(chunk: any, cb?: (() => void) | undefined): this;
   end(
@@ -100,17 +67,31 @@ export class StreamingServerResponse extends http.ServerResponse {
     cb?: (() => void) | undefined
   ): this;
   end(chunk?: unknown, encoding?: unknown, cb?: unknown): this {
-    if (chunk && typeof chunk !== "function") {
-      this[BODY].push(chunk);
-    }
     if (!this._wroteHeader) {
       // When next directly returns with end, the writeHead is not called,
       // so we need to call it here
-      this.writeHead(200);
+      this.writeHead(this.statusCode ?? 200);
     }
-    this._done = true;
+    if (chunk && typeof chunk !== "function") {
+      this.internalWrite(chunk);
+    }
+
+    setImmediate(() => {
+      if (!this._hasWritten) {
+        // We need to send data here, otherwise the stream will not end at all
+        this.internalWrite(new Uint8Array(8));
+      }
+      this.responseStream.end();
+    });
     debug("stream end", chunk);
     return this;
+  }
+
+  private internalWrite(chunk: any) {
+    setImmediate(() => {
+      this.responseStream.write(chunk);
+      this._hasWritten = true;
+    });
   }
 
   constructor(
@@ -122,7 +103,6 @@ export class StreamingServerResponse extends http.ServerResponse {
     super({ method });
 
     this[HEADERS] = {};
-    this[BODY] = [];
 
     this.fixHeaders = fixHeaders;
     this.responseStream = responseStream;
@@ -150,22 +130,7 @@ export class StreamingServerResponse extends http.ServerResponse {
           encoding = undefined;
         }
 
-        if (this._wroteHeader) {
-          this[BODY].push(data);
-        } else {
-          const string = getString(data);
-          const index = string.indexOf(headerEnd);
-
-          if (index !== -1) {
-            const remainder = string.slice(index + headerEnd.length);
-
-            if (remainder) {
-              this[BODY].push(data);
-            }
-
-            this._wroteHeader = true;
-          }
-        }
+        this.internalWrite(data);
 
         if (typeof cb === "function") {
           cb();
