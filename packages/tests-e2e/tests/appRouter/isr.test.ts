03349db4defa36bebc607a8d40573930db381912
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-08-31 08:53:29 -0700

    fix lint

--- packages/tests-e2e/tests/appRouter/isr.test.ts
+++ packages/tests-e2e/tests/appRouter/isr.test.ts
@@ -22,21 +22,21 @@ test("Incremental Static Regeneration", async ({ page }) => {
   } while (time !== newTime);
   await page.reload();
 
-  await wait(1000)
+  await wait(1000);
   el = page.getByText("ISR");
   const midTime = await el.textContent();
   // Expect that the time is still stale
-  expect(midTime).toEqual(newTime)
+  expect(midTime).toEqual(newTime);
 
   // Wait 10 + 1 seconds for ISR to regenerate time
   await wait(11000);
-  let finalTime = newTime
+  let finalTime = newTime;
   do {
-    await wait(2000)
+    await wait(2000);
     el = page.getByText("ISR");
-    finalTime = await el.textContent()
+    finalTime = await el.textContent();
     await page.reload();
-  } while (newTime === finalTime)
+  } while (newTime === finalTime);
 
   expect(newTime).not.toEqual(finalTime);
 });
