ddd23cfa88ca24cb9363daf2d913ff8f2fe41998
khuezy <khuezy.nguyen@gmail.com>
2023-08-31 07:57:30 -0700

    update next.config (#201)

--- examples/app-router/next.config.js
+++ examples/app-router/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  output: "standalone",
   transpilePackages: ["@example/shared"],
-  outputFileTracing: "../sst",
   experimental: {
     serverActions: true,
   },
--- examples/pages-router/next.config.js
+++ examples/pages-router/next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   transpilePackages: ["@example/shared"],
   reactStrictMode: true,
-  output: "standalone",
-  outputFileTracing: "../sst",
 };
 
 module.exports = nextConfig;
