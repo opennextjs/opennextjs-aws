d2007894b57894dde6e91022ade848451401560f
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-09-01 08:09:47 -0700

    update e2e tests

--- packages/tests-e2e/tests/appRouter/ssr.test.ts
+++ packages/tests-e2e/tests/appRouter/ssr.test.ts
@@ -1,10 +1,10 @@
-// NOTE: loading.tsx is currently broken on open-next
-//  This works locally but not on deployed apps
+// // NOTE: loading.tsx is currently broken on open - next
+// //  This works locally but not on deployed apps
 
 // import { test, expect } from "@playwright/test";
 // import { wait } from "@open-next/utils";
 
-// NOTE: We don't await page load b/c we want to see the Loading page
+// // NOTE: We don't await page load b/c we want to see the Loading page
 // test("Server Side Render and loading.tsx", async ({ page }) => {
 //   await page.goto("/");
 //   await page.getByRole("link", { name: "SSR" }).click();
