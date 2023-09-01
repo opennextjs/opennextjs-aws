2600a391c44b7c97972bd03424f70adde3e40e89
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-09-01 15:11:58 -0700

    Rebase main and add unit tests

---
+++ packages/tests-unit/tests/utils.test.ts
@@ -0,0 +1,41 @@
+import { fixSWRCacheHeader } from "@open-next/utils";
+
+describe("Utils", () => {
+  describe("fixSWRCacheHeader", () => {
+    it("Does not update swr if already set", () => {
+      const headers = {
+        "cache-control": "maxage=69,stale-while-revalidate=11",
+      };
+      fixSWRCacheHeader(headers);
+
+      expect(headers).toEqual({
+        "cache-control": "maxage=69,stale-while-revalidate=11",
+      });
+      console.log(headers);
+    });
+
+    it("Does not add swr if not exists", () => {
+      const headers = {
+        "cache-control": "maxage=69",
+      };
+      fixSWRCacheHeader(headers);
+
+      expect(headers).toEqual({
+        "cache-control": "maxage=69",
+      });
+      console.log(headers);
+    });
+
+    it("Sets time on swf if not defined", () => {
+      const headers = {
+        "cache-control": "maxage=69,stale-while-revalidate",
+      };
+      fixSWRCacheHeader(headers);
+
+      expect(headers).toEqual({
+        "cache-control": "maxage=69,stale-while-revalidate=2592000",
+      });
+      console.log(headers);
+    });
+  });
+});
