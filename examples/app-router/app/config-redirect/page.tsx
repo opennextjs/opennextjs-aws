aa36c99c5da36b9e10916aea84d81245fcf9a0b3
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-10-07 10:18:14 -0700

    fix cookie redirect matcher

---
+++ examples/app-router/app/config-redirect/page.tsx
@@ -0,0 +1,8 @@
+export default function RedirectDestination() {
+  return (
+    <div>
+      <h1>I was redirected from next.config.js</h1>
+      <p>/next-config-redirect =&gt; /config-redirect</p>
+    </div>
+  );
+}
---
+++ packages/tests-e2e/tests/appRouter/config.redirect.test.ts
@@ -0,0 +1,63 @@
+import { expect, test } from "@playwright/test";
+/**
+ * This tests that the "redirect" config in next.config.js works
+ * 
+ * redirects: () => {
+    return [
+      {
+        source: "/next-config-redirect",
+        destination: "/config-redirect",
+        permanent: true,
+        missing: [{ type: "cookie", key: "missing-cookie" }],
+      },
+    ];
+  },
+ */
+test.describe("Next Config Redirect", () => {
+  test("Missing cookies", async ({ page }) => {
+    await page.goto("/");
+    await page.goto("/next-config-redirect-missing");
+
+    await page.waitForURL(`/config-redirect?missing=true`);
+
+    const el = page.getByText("I was redirected from next.config.js", {
+      exact: true,
+    });
+    await expect(el).toBeVisible();
+  });
+  test("Has cookies", async ({ page }) => {
+    await page.goto("/");
+    await page.goto("/next-config-redirect-has");
+
+    await page.waitForURL(`/config-redirect?has=true`);
+
+    const el = page.getByText("I was redirected from next.config.js", {
+      exact: true,
+    });
+    await expect(el).toBeVisible();
+  });
+  test("Has cookies with value", async ({ page }) => {
+    await page.goto("/");
+    await page.goto("/next-config-redirect-has-with-value");
+
+    await page.waitForURL(`/config-redirect?hasWithValue=true`);
+
+    const el = page.getByText("I was redirected from next.config.js", {
+      exact: true,
+    });
+    await expect(el).toBeVisible();
+  });
+  test("Has cookies with bad value", async ({ page }) => {
+    await page.goto("/");
+    await page.goto("/next-config-redirect-has-with-bad-value");
+
+    // did not redirect
+    await page.waitForURL(`/next-config-redirect-has-with-bad-value`);
+
+    // 404 not found
+    const el = page.getByText("This page could not be found.", {
+      exact: true,
+    });
+    await expect(el).toBeVisible();
+  });
+});
