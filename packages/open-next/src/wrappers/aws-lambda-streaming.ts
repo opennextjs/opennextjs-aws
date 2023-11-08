2ca0285ac248ff45b75514915ebccc29666718ed
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2023-11-08 10:52:00 +0100

    compression support

--- packages/open-next/src/wrappers/aws-lambda-streaming.ts
+++ packages/open-next/src/wrappers/aws-lambda-streaming.ts
@@ -1,3 +1,6 @@
+import { Writable } from "node:stream";
+import zlib from "node:zlib";
+
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 
 import { StreamCreator } from "../adapters/http/openNextResponse";
@@ -16,6 +19,35 @@ const handler: Wrapper = async (handler, converter) =>
       let _hasWriten = false;
       let _headersSent = false;
 
+      //Handle compression
+      const acceptEncoding = internalEvent.headers["accept-encoding"] ?? "";
+      let contentEncoding: string;
+      let compressedStream: Writable | undefined;
+
+      if (acceptEncoding.includes("br")) {
+        contentEncoding = "br";
+        compressedStream = zlib.createBrotliCompress({
+          flush: zlib.constants.BROTLI_OPERATION_FLUSH,
+          finishFlush: zlib.constants.BROTLI_OPERATION_FINISH,
+        });
+        compressedStream.pipe(responseStream);
+      } else if (acceptEncoding.includes("gzip")) {
+        contentEncoding = "gzip";
+        compressedStream = zlib.createGzip({
+          flush: zlib.constants.Z_SYNC_FLUSH,
+        });
+        compressedStream.pipe(responseStream);
+      } else if (acceptEncoding.includes("deflate")) {
+        contentEncoding = "deflate";
+        compressedStream = zlib.createDeflate({
+          flush: zlib.constants.Z_SYNC_FLUSH,
+        });
+        compressedStream.pipe(responseStream);
+      } else {
+        contentEncoding = "identity";
+        compressedStream = responseStream;
+      }
+
       const streamCreator: StreamCreator = {
         writeHeaders: (_prelude) => {
           responseStream.cork();
@@ -24,9 +56,11 @@ const handler: Wrapper = async (handler, converter) =>
           // We replace their write with ours which are inside a setImmediate
           // This way it seems to work all the time
           // I think we can't ship this code as it is, it could break at anytime if they decide to change the runtime and they already did it in the past
+
           responseStream.setContentType(
             "application/vnd.awslambda.http-integration-response",
           );
+          _prelude.headers["content-encoding"] = contentEncoding;
           const prelude = JSON.stringify(_prelude);
 
           // Try to flush the buffer to the client to invoke
@@ -46,21 +80,21 @@ const handler: Wrapper = async (handler, converter) =>
 
           _headersSent = true;
 
-          return responseStream;
+          return compressedStream ?? responseStream;
         },
         onWrite: () => {
           _hasWriten = true;
           // Force flushing data, seems necessary for aws lambda streaming to work reliably
           // We need to reevaluate this if it causes issues on other platforms
-          if (responseStream?.writableCorked) {
-            responseStream?.uncork();
+          if (compressedStream?.writableCorked) {
+            compressedStream?.uncork();
           }
         },
       };
 
       const response = await handler(internalEvent, streamCreator);
 
-      if (!responseStream.writableFinished) {
+      if (!compressedStream.writableFinished) {
         // If the headers are not sent, we need to send them
         if (!_headersSent) {
           streamCreator.writeHeaders({
@@ -69,7 +103,7 @@ const handler: Wrapper = async (handler, converter) =>
             headers: parseHeaders(response?.headers),
           });
         }
-        responseStream.end(_hasWriten ? undefined : new Uint8Array(8));
+        compressedStream.end(_hasWriten ? undefined : new Uint8Array(8));
       }
 
       return converter.convertTo(response);
