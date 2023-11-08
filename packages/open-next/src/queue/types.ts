a9375ecb6cbaf33903067f98644af2a2145e6a71
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2023-11-08 13:40:39 +0100

    overridable queue

---
+++ packages/open-next/src/queue/types.ts
@@ -0,0 +1,12 @@
+export interface QueueMessage {
+  MessageDeduplicationId: string;
+  MessageBody: {
+    host: string;
+    url: string;
+  };
+  MessageGroupId: string;
+}
+
+export interface Queue {
+  send(message: QueueMessage): Promise<void>;
+}
