0608f27209bbc02e8c940c82fa700954b18971ac
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2023-11-08 13:00:12 +0100

    add converter for apigw-v1 & cloudfront

---
+++ packages/open-next/src/converters/aws-cloudfront.ts
@@ -0,0 +1,87 @@
+import {
+  CloudFrontHeaders,
+  CloudFrontRequestEvent,
+  CloudFrontRequestResult,
+} from "aws-lambda";
+
+import { InternalEvent, InternalResult } from "../adapters/event-mapper";
+import { debug } from "../adapters/logger";
+import { Converter } from "../adapters/types/open-next";
+
+function normalizeCloudFrontRequestEventHeaders(
+  rawHeaders: CloudFrontHeaders,
+): Record<string, string> {
+  const headers: Record<string, string> = {};
+
+  for (const [key, values] of Object.entries(rawHeaders)) {
+    for (const { value } of values) {
+      if (value) {
+        headers[key.toLowerCase()] = value;
+      }
+    }
+  }
+
+  return headers;
+}
+
+async function convertFromCloudFrontRequestEvent(
+  event: CloudFrontRequestEvent,
+): Promise<InternalEvent> {
+  const { method, uri, querystring, body, headers, clientIp } =
+    event.Records[0].cf.request;
+  return {
+    type: "cf",
+    method,
+    rawPath: uri,
+    url: uri + (querystring ? `?${querystring}` : ""),
+    body: Buffer.from(
+      body?.data ?? "",
+      body?.encoding === "base64" ? "base64" : "utf8",
+    ),
+    headers: normalizeCloudFrontRequestEventHeaders(headers),
+    remoteAddress: clientIp,
+    query: querystring.split("&").reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur.split("=")[0]]: cur.split("=")[1],
+      }),
+      {},
+    ),
+    cookies:
+      headers.cookie?.reduce((acc, cur) => {
+        const { key, value } = cur;
+        return { ...acc, [key ?? ""]: value };
+      }, {}) ?? {},
+  };
+}
+
+function convertToCloudFrontRequestResult(
+  result: InternalResult,
+): CloudFrontRequestResult {
+  const headers: CloudFrontHeaders = {};
+  Object.entries(result.headers)
+    .filter(([key]) => key.toLowerCase() !== "content-length")
+    .forEach(([key, value]) => {
+      headers[key] = [
+        ...(headers[key] || []),
+        ...(Array.isArray(value)
+          ? value.map((v) => ({ key, value: v }))
+          : [{ key, value: value.toString() }]),
+      ];
+    });
+
+  const response: CloudFrontRequestResult = {
+    status: result.statusCode.toString(),
+    statusDescription: "OK",
+    headers,
+    bodyEncoding: result.isBase64Encoded ? "base64" : "text",
+    body: result.body,
+  };
+  debug(response);
+  return response;
+}
+
+export default {
+  convertFrom: convertFromCloudFrontRequestEvent,
+  convertTo: convertToCloudFrontRequestResult,
+} as Converter;
