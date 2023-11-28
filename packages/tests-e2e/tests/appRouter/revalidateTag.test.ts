opennextjs/opennextjs-aws

commit b7c8f47a21562593fd0df294d2b4edaa12c69df8

Perf Reduce s3 calls (#295)
* merge cache files into a single file

* incremental cache use the single cache file

* update doc

packages/open-next/src/adapters/cache.ts
@@ -9,7 +9,6 @@ import {
   ListObjectsV2Command,
   PutObjectCommand,
   PutObjectCommandInput,
-  PutObjectCommandOutput,
   S3Client,
 } from "@aws-sdk/client-s3";
 import path from "path";
@@ -88,17 +87,38 @@ interface CacheHandlerValue {
   value: IncrementalCacheValue | null;
 }
 
-type CacheExtension =
-  | "json"
-  | "html"
-  | "rsc"
-  | "body"
-  | "meta"
-  | "fetch"
-  | "redirect";
+type Extension = "cache" | "fetch";
+
+interface Meta {
+  status?: number;
+  headers?: Record<string, undefined | string | string[]>;
+}
+type S3CachedFile =
+  | {
+      type: "redirect";
+      props?: Object;
+      meta?: Meta;
+    }
+  | {
+      type: "page";
+      html: string;
+      json: Object;
+      meta?: Meta;
+    }
+  | {
+      type: "app";
+      html: string;
+      rsc: string;
+      meta?: Meta;
+    }
+  | {
+      type: "route";
+      body: string;
+      meta?: Meta;
+    };
 
 /** Beginning single backslash is intentional, to look for the dot + the extension. Do not escape it again. */
-const CACHE_EXTENSION_REGEX = /\.(json|html|rsc|body|meta|fetch|redirect)$/;
+const CACHE_EXTENSION_REGEX = /\.(cache|fetch)$/;
 
 export function hasCacheExtension(key: string) {
   return CACHE_EXTENSION_REGEX.test(key);
@@ -136,18 +156,15 @@ export default class S3Cache {
     }
     const isFetchCache =
       typeof options === "object" ? options.fetchCache : options;
-    const keys = await this.listS3Object(key);
-    if (keys.length === 0) return null;
-    debug("keys", keys);
     return isFetchCache
-      ? this.getFetchCache(key, keys)
-      : this.getIncrementalCache(key, keys);
+      ? this.getFetchCache(key)
+      : this.getIncrementalCache(key);
   }
 
-  async getFetchCache(key: string, keys: string[]) {
+  async getFetchCache(key: string) {
     debug("get fetch cache", { key });
     try {
-      const { Body, LastModified } = await this.getS3Object(key, "fetch", keys);
+      const { Body, LastModified } = await this.getS3Object(key, "fetch");
       const lastModified = await this.getHasRevalidatedTags(
         key,
         LastModified?.getTime(),
@@ -169,96 +186,59 @@ export default class S3Cache {
     }
   }
 
-  async getIncrementalCache(
-    key: string,
-    keys: string[],
-  ): Promise<CacheHandlerValue | null> {
-    if (keys.includes(this.buildS3Key(key, "body"))) {
-      debug("get body cache ", { key });
-      try {
-        const [{ Body, LastModified }, { Body: MetaBody }] = await Promise.all([
-          this.getS3Object(key, "body", keys),
-          this.getS3Object(key, "meta", keys),
-        ]);
-        const body = await Body?.transformToByteArray();
-        const meta = JSON.parse((await MetaBody?.transformToString()) ?? "{}");
-
+  async getIncrementalCache(key: string): Promise<CacheHandlerValue | null> {
+    try {
+      const { Body, LastModified } = await this.getS3Object(key, "cache");
+      const cacheData = JSON.parse(
+        (await Body?.transformToString()) ?? "{}",
+      ) as S3CachedFile;
+      const meta = cacheData.meta;
+      const lastModified = await this.getHasRevalidatedTags(
+        key,
+        LastModified?.getTime(),
+      );
+      if (lastModified === -1) {
+        // If some tags are stale we need to force revalidation
+        return null;
+      }
+      if (cacheData.type === "route") {
         return {
           lastModified: LastModified?.getTime(),
           value: {
             kind: "ROUTE",
-            body: Buffer.from(body ?? Buffer.alloc(0)),
-            status: meta.status,
-            headers: meta.headers,
+            body: Buffer.from(cacheData.body ?? Buffer.alloc(0)),
+            status: meta?.status,
+            headers: meta?.headers,
           },
         } as CacheHandlerValue;
-      } catch (e) {
-        error("Failed to get body cache", e);
-      }
-      return null;
-    }
-
-    if (keys.includes(this.buildS3Key(key, "html"))) {
-      const isJson = keys.includes(this.buildS3Key(key, "json"));
-      const isRsc = keys.includes(this.buildS3Key(key, "rsc"));
-      debug("get html cache ", { key, isJson, isRsc });
-      if (!isJson && !isRsc) return null;
-
-      try {
-        const [{ Body, LastModified }, { Body: PageBody }, { Body: MetaBody }] =
-          await Promise.all([
-            this.getS3Object(key, "html", keys),
-            this.getS3Object(key, isJson ? "json" : "rsc", keys),
-            this.getS3Object(key, "meta", keys),
-          ]);
-        const lastModified = await this.getHasRevalidatedTags(
-          key,
-          LastModified?.getTime(),
-        );
-        if (lastModified === -1) {
-          // If some tags are stale we need to force revalidation
-          return null;
-        }
-        const meta = JSON.parse((await MetaBody?.transformToString()) ?? "{}");
-        return {
-          lastModified,
-          value: {
-            kind: "PAGE",
-            html: (await Body?.transformToString()) ?? "",
-            pageData: isJson
-              ? JSON.parse((await PageBody?.transformToString()) ?? "{}")
-              : await PageBody?.transformToString(),
-            status: meta.status,
-            headers: meta.headers,
-          },
-        } as CacheHandlerValue;
-      } catch (e) {
-        error("Failed to get html cache", e);
-      }
-      return null;
-    }
-
-    // Check for redirect last. This way if a page has been regenerated
-    // after having been redirected, we'll get the page data
-    if (keys.includes(this.buildS3Key(key, "redirect"))) {
-      debug("get redirect cache", { key });
-      try {
-        const { Body, LastModified } = await this.getS3Object(
-          key,
-          "redirect",
-          keys,
-        );
+      } else if (cacheData.type === "page" || cacheData.type === "app") {
         return {
           lastModified: LastModified?.getTime(),
-          value: JSON.parse((await Body?.transformToString()) ?? "{}"),
-        };
-      } catch (e) {
-        error("Failed to get redirect cache", e);
+          value: {
+            kind: "PAGE",
+            html: cacheData.html,
+            pageData:
+              cacheData.type === "page" ? cacheData.json : cacheData.rsc,
+            status: meta?.status,
+            headers: meta?.headers,
+          },
+        } as CacheHandlerValue;
+      } else if (cacheData.type === "redirect") {
+        return {
+          lastModified: LastModified?.getTime(),
+          value: {
+            kind: "REDIRECT",
+            props: cacheData.props,
+          },
+        } as CacheHandlerValue;
+      } else {
+        error("Unknown cache type", cacheData);
+        return null;
       }
+    } catch (e) {
+      error("Failed to get body cache", e);
       return null;
     }
-
-    return null;
   }
 
   async set(key: string, data?: IncrementalCacheValue): Promise<void> {
@@ -267,37 +247,44 @@ export default class S3Cache {
     }
     if (data?.kind === "ROUTE") {
       const { body, status, headers } = data;
-      await Promise.all([
-        this.putS3Object(key, "body", body),
-        this.putS3Object(key, "meta", JSON.stringify({ status, headers })),
-      ]);
+      this.putS3Object(
+        key,
+        "cache",
+        JSON.stringify({
+          type: "route",
+          body: body.toString("utf8"),
+          meta: {
+            status,
+            headers,
+          },
+        } as S3CachedFile),
+      );
     } else if (data?.kind === "PAGE") {
       const { html, pageData } = data;
       const isAppPath = typeof pageData === "string";
-      let metaPromise: Promise<PutObjectCommandOutput | void> =
-        Promise.resolve();
-      if (data.status || data.headers) {
-        metaPromise = this.putS3Object(
-          key,
-          "meta",
-          JSON.stringify({ status: data.status, headers: data.headers }),
-        );
-      }
-      await Promise.all([
-        this.putS3Object(key, "html", html),
-        this.putS3Object(
-          key,
-          isAppPath ? "rsc" : "json",
-          isAppPath ? pageData : JSON.stringify(pageData),
-        ),
-        metaPromise,
-      ]);
+      this.putS3Object(
+        key,
+        "cache",
+        JSON.stringify({
+          type: isAppPath ? "app" : "page",
+          html,
+          rsc: isAppPath ? pageData : undefined,
+          json: isAppPath ? undefined : pageData,
+          meta: { status: data.status, headers: data.headers },
+        } as S3CachedFile),
+      );
     } else if (data?.kind === "FETCH") {
       await this.putS3Object(key, "fetch", JSON.stringify(data));
     } else if (data?.kind === "REDIRECT") {
-      // delete potential page data if we're redirecting
-      await this.deleteS3Objects(key);
-      await this.putS3Object(key, "redirect", JSON.stringify(data));
+      // // delete potential page data if we're redirecting
+      await this.putS3Object(
+        key,
+        "cache",
+        JSON.stringify({
+          type: "redirect",
+          props: data.props,
+        } as S3CachedFile),
+      );
     } else if (data === null || data === undefined) {
       await this.deleteS3Objects(key);
     }
@@ -466,7 +453,7 @@ export default class S3Cache {
 
   // S3 handling
 
-  private buildS3Key(key: string, extension: CacheExtension) {
+  private buildS3Key(key: string, extension: Extension) {
     return path.posix.join(
       CACHE_BUCKET_KEY_PREFIX ?? "",
       extension === "fetch" ? "__fetch" : "",
@@ -491,14 +478,8 @@ export default class S3Cache {
     return (Contents ?? []).map(({ Key }) => Key) as string[];
   }
 
-  private async getS3Object(
-    key: string,
-    extension: CacheExtension,
-    keys: string[],
-  ) {
+  private async getS3Object(key: string, extension: Extension) {
     try {
-      if (!keys.includes(this.buildS3Key(key, extension)))
-        return { Body: null, LastModified: null };
       const result = await this.client.send(
         new GetObjectCommand({
           Bucket: CACHE_BUCKET_NAME,
@@ -514,7 +495,7 @@ export default class S3Cache {
 
   private putS3Object(
     key: string,
-    extension: CacheExtension,
+    extension: Extension,
     value: PutObjectCommandInput["Body"],
   ) {
     return this.client.send(

packages/open-next/src/build.ts
@@ -431,6 +431,62 @@ function createCacheAssets(monorepoRoot: string, disableDynamoDBCache = false) {
       (file.endsWith(".html") && htmlPages.has(file)),
   );
 
+  //merge cache files into a single file
+  const cacheFilesPath: Record<
+    string,
+    {
+      meta?: string;
+      html?: string;
+      json?: string;
+      rsc?: string;
+      body?: string;
+    }
+  > = {};
+
+  traverseFiles(
+    outputPath,
+    () => true,
+    (filepath) => {
+      const ext = path.extname(filepath);
+      const newFilePath =
+        ext !== "" ? filepath.replace(ext, ".cache") : `${filepath}.cache`;
+      switch (ext) {
+        case ".meta":
+        case ".html":
+        case ".json":
+        case ".body":
+        case ".rsc":
+          cacheFilesPath[newFilePath] = {
+            [ext.slice(1)]: filepath,
+            ...cacheFilesPath[newFilePath],
+          };
+          break;
+        default:
+          console.warn(`Unknown file extension: ${ext}`);
+          break;
+      }
+    },
+  );
+
+  // Generate cache file
+  Object.entries(cacheFilesPath).forEach(([cacheFilePath, files]) => {
+    const cacheFileContent = {
+      type: files.body ? "route" : files.json ? "page" : "app",
+      meta: files.meta
+        ? JSON.parse(fs.readFileSync(files.meta, "utf8"))
+        : undefined,
+      html: files.html ? fs.readFileSync(files.html, "utf8") : undefined,
+      json: files.json
+        ? JSON.parse(fs.readFileSync(files.json, "utf8"))
+        : undefined,
+      rsc: files.rsc ? fs.readFileSync(files.rsc, "utf8") : undefined,
+      body: files.body ? fs.readFileSync(files.body, "utf8") : undefined,
+    };
+    fs.writeFileSync(cacheFilePath, JSON.stringify(cacheFileContent));
+  });
+
+  removeFiles(outputPath, (file) => !file.endsWith(".cache"));
+
   if (!disableDynamoDBCache) {
     // Generate dynamodb data
     // We need to traverse the cache to find every .meta file

docs/pages/inner_workings/isr.mdx
@@ -44,22 +44,21 @@ They can also be called on fetch requests if the `cache` option is not set to `n
 
 There is also some cost associated to deployment since you need to upload the cache to S3 and upload the tags to DynamoDB.
 
-For the examples here, let's assume an app route with a 5 minute revalidation delay in us-east-1 (App uses 3 instead of 2 `GetObject`). This is assuming you get constant traffic to the route (If you get no traffic, you will only pay for the storage cost).
+For the examples here, let's assume an app route with a 5 minute revalidation delay in us-east-1. This is assuming you get constant traffic to the route (If you get no traffic, you will only pay for the storage cost).
 
 ##### S3
-- Each `get` request to the cache will result in at least 1 `ListRequest` in S3 and between 2 and 3 `GetObject`
+- Each `get` request to the cache will result in at least 1 `GetObject`
 
 ```
-  List Request cost - 8,640 requests * $0.005 per 1,000 requests = $0.0432
-  GetObject cost - 8,640 requests * $0.0004 per 1,000 requests * 3 = $0.010368
-  Total cost - $0.053568 per route per month
+  GetObject cost - 8,640 requests * $0.0004 per 1,000 requests  = $0.003456
+  Total cost - $0.003456 per route per month
 ```
 
-- Each `set` request to the cache will result in 2 to 3 `PutObject` in S3
+- Each `set` request to the cache will result in 1 `PutObject` in S3
 
 ```
-  PutObject cost - 8,640 requests * $0.005 per 1,000 requests * 3 = $0.1296
-  Total cost - $0.1296 per route per month
+  PutObject cost - 8,640 requests * $0.005 per 1,000 requests = $0.0432
+  Total cost - $0.0432 per route per month
 ```
 
 You can then calculate the cost based on your usage and the [S3 pricing](https://aws.amazon.com/s3/pricing/)

commit abeb9cdb91f8e3a1efbb9a8b66b910a5e1ce4699

Fix tags cache (#317)
* fix: fetchCache for next 14+

* fix: regression from #295, tags were not generated

* test: add test for revalidateTag

* pin next version

* add changeset

examples/app-router/app/api/revalidate-tag/route.ts
@@ -1,0 +1,7 @@
+import { revalidateTag } from "next/cache";
+
+export async function GET() {
+  revalidateTag("revalidate");
+
+  return new Response("ok");
+}

examples/app-router/app/revalidate-tag/layout.tsx
@@ -1,0 +1,19 @@
+import { unstable_cache } from "next/cache";
+import type { ReactNode } from "react";
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const fakeFetch = unstable_cache(
+    async () => new Date().getTime(),
+    ["fakeFetch"],
+    {
+      tags: ["revalidate"],
+    },
+  );
+  const fetchedDate = await fakeFetch();
+  return (
+    <div>
+      <div>Fetched time: {new Date(fetchedDate).toISOString()}</div>
+      {children}
+    </div>
+  );
+}

examples/app-router/app/revalidate-tag/nested/page.tsx
@@ -1,0 +1,3 @@
+export default async function Nested() {
+  return <div>Nested</div>;
+}

examples/app-router/app/revalidate-tag/page.tsx
@@ -1,0 +1,8 @@
+async function getTime() {
+  return new Date().toISOString();
+}
+
+export default async function ISR() {
+  const time = getTime();
+  return <div>Time: {time}</div>;
+}

examples/app-router/middleware.ts
@@ -42,6 +42,14 @@ export function middleware(request: NextRequest) {
     );
   }
 
+  // It is so that cloudfront doesn't cache the response
+  if (path.startsWith("/revalidate-tag")) {
+    responseHeaders.set(
+      "cache-control",
+      "private, no-cache, no-store, max-age=0, must-revalidate",
+    );
+  }
+
   const r = NextResponse.next({
     headers: responseHeaders,
     request: {

packages/open-next/src/adapters/cache.ts
@@ -159,12 +159,22 @@ export default class S3Cache {
     this.buildId = NEXT_BUILD_ID!;
   }
 
-  public async get(key: string, options?: boolean | { fetchCache?: boolean }) {
+  public async get(
+    key: string,
+    // fetchCache is for next 13.5 and above, kindHint is for next 14 and above and boolean is for earlier versions
+    options?:
+      | boolean
+      | { fetchCache?: boolean; kindHint?: "app" | "pages" | "fetch" },
+  ) {
     if (globalThis.disableIncrementalCache) {
       return null;
     }
     const isFetchCache =
-      typeof options === "object" ? options.fetchCache : options;
+      typeof options === "object"
+        ? options.kindHint
+          ? options.kindHint === "fetch"
+          : options.fetchCache
+        : options;
     return isFetchCache
       ? this.getFetchCache(key)
       : this.getIncrementalCache(key);

packages/open-next/src/build.ts
@@ -509,8 +509,6 @@ function createCacheAssets(monorepoRoot: string, disableDynamoDBCache = false) {
     fs.writeFileSync(cacheFilePath, JSON.stringify(cacheFileContent));
   });
 
-  removeFiles(outputPath, (file) => !file.endsWith(".cache"));
-
   if (!disableDynamoDBCache) {
     // Generate dynamodb data
     // We need to traverse the cache to find every .meta file
@@ -597,6 +595,9 @@ function createCacheAssets(monorepoRoot: string, disableDynamoDBCache = false) {
       );
     }
   }
+
+  // We need to remove files later because we need the metafiles for dynamodb tags cache
+  removeFiles(outputPath, (file) => !file.endsWith(".cache"));
 }
 
 /***************************/

pnpm-lock.yaml
@@ -103,8 +103,8 @@ importers:
         specifier: workspace:*
         version: link:../../packages/utils
       next:
-        specifier: latest
-        version: 13.5.3(react-dom@18.2.0)(react@18.2.0)
+        specifier: ^14.0.3
+        version: 14.0.3(react-dom@18.2.0)(react@18.2.0)
       open-next:
         specifier: workspace:*
         version: link:../../packages/open-next
@@ -146,8 +146,8 @@ importers:
         specifier: workspace:*
         version: link:../../packages/utils
       next:
-        specifier: latest
-        version: 13.5.3(react-dom@18.2.0)(react@18.2.0)
+        specifier: ^14.0.3
+        version: 14.0.3(react-dom@18.2.0)(react@18.2.0)
       open-next:
         specifier: workspace:*
         version: link:../../packages/open-next
@@ -198,8 +198,8 @@ importers:
         specifier: 10.4.15
         version: 10.4.15(postcss@8.4.27)
       next:
-        specifier: latest
-        version: 13.5.3(react-dom@18.2.0)(react@18.2.0)
+        specifier: ^14.0.3
+        version: 14.0.3(react-dom@18.2.0)(react@18.2.0)
       postcss:
         specifier: 8.4.27
         version: 8.4.27
@@ -469,6 +469,9 @@ packages:
   /@aws-cdk/cloud-assembly-schema@2.101.1:
     resolution: {integrity: sha512-zP+5eaOcnEMTZHcVSl8oqrzttKpLm4i1yEMkh7mwbVVAcH6ofd4sPKc8LDkJRWruP47Z9yDtiwx+ly2ZRXG58Q==}
     engines: {node: '>= 14.15.0'}
+    dependencies:
+      jsonschema: 1.4.1
+      semver: 7.5.4
     dev: true
     bundledDependencies:
       - jsonschema
@@ -516,6 +519,7 @@ packages:
       '@aws-cdk/cloud-assembly-schema': 2.101.1
     dependencies:
       '@aws-cdk/cloud-assembly-schema': 2.101.1
+      semver: 7.5.4
     dev: true
     bundledDependencies:
       - semver
@@ -4727,8 +4731,8 @@ packages:
     resolution: {integrity: sha512-RmHanbV21saP/6OEPBJ7yJMuys68cIf8OBBWd7+uj40LdpmswVAwe1uzeuFyUsd6SfeITWT3XnQfn6wULeKwDQ==}
     dev: false
 
-  /@next/env@13.5.3:
-    resolution: {integrity: sha512-X4te86vsbjsB7iO4usY9jLPtZ827Mbx+WcwNBGUOIuswuTAKQtzsuoxc/6KLxCMvogKG795MhrR1LDhYgDvasg==}
+  /@next/env@14.0.3:
+    resolution: {integrity: sha512-7xRqh9nMvP5xrW4/+L0jgRRX+HoNRGnfJpD+5Wq6/13j3dsdzxO3BCXn7D3hMqsDb+vjZnJq+vI7+EtgrYZTeA==}
     dev: false
 
   /@next/eslint-plugin-next@13.4.19:
@@ -4746,8 +4750,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-darwin-arm64@13.5.3:
-    resolution: {integrity: sha512-6hiYNJxJmyYvvKGrVThzo4nTcqvqUTA/JvKim7Auaj33NexDqSNwN5YrrQu+QhZJCIpv2tULSHt+lf+rUflLSw==}
+  /@next/swc-darwin-arm64@14.0.3:
+    resolution: {integrity: sha512-64JbSvi3nbbcEtyitNn2LEDS/hcleAFpHdykpcnrstITFlzFgB/bW0ER5/SJJwUPj+ZPY+z3e+1jAfcczRLVGw==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [darwin]
@@ -4764,8 +4768,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-darwin-x64@13.5.3:
-    resolution: {integrity: sha512-UpBKxu2ob9scbpJyEq/xPgpdrgBgN3aLYlxyGqlYX5/KnwpJpFuIHU2lx8upQQ7L+MEmz+fA1XSgesoK92ppwQ==}
+  /@next/swc-darwin-x64@14.0.3:
+    resolution: {integrity: sha512-RkTf+KbAD0SgYdVn1XzqE/+sIxYGB7NLMZRn9I4Z24afrhUpVJx6L8hsRnIwxz3ERE2NFURNliPjJ2QNfnWicQ==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [darwin]
@@ -4782,8 +4786,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-linux-arm64-gnu@13.5.3:
-    resolution: {integrity: sha512-5AzM7Yx1Ky+oLY6pHs7tjONTF22JirDPd5Jw/3/NazJ73uGB05NqhGhB4SbeCchg7SlVYVBeRMrMSZwJwq/xoA==}
+  /@next/swc-linux-arm64-gnu@14.0.3:
+    resolution: {integrity: sha512-3tBWGgz7M9RKLO6sPWC6c4pAw4geujSwQ7q7Si4d6bo0l6cLs4tmO+lnSwFp1Tm3lxwfMk0SgkJT7EdwYSJvcg==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
@@ -4800,8 +4804,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-linux-arm64-musl@13.5.3:
-    resolution: {integrity: sha512-A/C1shbyUhj7wRtokmn73eBksjTM7fFQoY2v/0rTM5wehpkjQRLOXI8WJsag2uLhnZ4ii5OzR1rFPwoD9cvOgA==}
+  /@next/swc-linux-arm64-musl@14.0.3:
+    resolution: {integrity: sha512-v0v8Kb8j8T23jvVUWZeA2D8+izWspeyeDGNaT2/mTHWp7+37fiNfL8bmBWiOmeumXkacM/AB0XOUQvEbncSnHA==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
@@ -4818,8 +4822,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-linux-x64-gnu@13.5.3:
-    resolution: {integrity: sha512-FubPuw/Boz8tKkk+5eOuDHOpk36F80rbgxlx4+xty/U71e3wZZxVYHfZXmf0IRToBn1Crb8WvLM9OYj/Ur815g==}
+  /@next/swc-linux-x64-gnu@14.0.3:
+    resolution: {integrity: sha512-VM1aE1tJKLBwMGtyBR21yy+STfl0MapMQnNrXkxeyLs0GFv/kZqXS5Jw/TQ3TSUnbv0QPDf/X8sDXuMtSgG6eg==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
@@ -4836,8 +4840,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-linux-x64-musl@13.5.3:
-    resolution: {integrity: sha512-DPw8nFuM1uEpbX47tM3wiXIR0Qa+atSzs9Q3peY1urkhofx44o7E1svnq+a5Q0r8lAcssLrwiM+OyJJgV/oj7g==}
+  /@next/swc-linux-x64-musl@14.0.3:
+    resolution: {integrity: sha512-64EnmKy18MYFL5CzLaSuUn561hbO1Gk16jM/KHznYP3iCIfF9e3yULtHaMy0D8zbHfxset9LTOv6cuYKJgcOxg==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
@@ -4854,8 +4858,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-win32-arm64-msvc@13.5.3:
-    resolution: {integrity: sha512-zBPSP8cHL51Gub/YV8UUePW7AVGukp2D8JU93IHbVDu2qmhFAn9LWXiOOLKplZQKxnIPUkJTQAJDCWBWU4UWUA==}
+  /@next/swc-win32-arm64-msvc@14.0.3:
+    resolution: {integrity: sha512-WRDp8QrmsL1bbGtsh5GqQ/KWulmrnMBgbnb+59qNTW1kVi1nG/2ndZLkcbs2GX7NpFLlToLRMWSQXmPzQm4tog==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [win32]
@@ -4872,8 +4876,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-win32-ia32-msvc@13.5.3:
-    resolution: {integrity: sha512-ONcL/lYyGUj4W37D4I2I450SZtSenmFAvapkJQNIJhrPMhzDU/AdfLkW98NvH1D2+7FXwe7yclf3+B7v28uzBQ==}
+  /@next/swc-win32-ia32-msvc@14.0.3:
+    resolution: {integrity: sha512-EKffQeqCrj+t6qFFhIFTRoqb2QwX1mU7iTOvMyLbYw3QtqTw9sMwjykyiMlZlrfm2a4fA84+/aeW+PMg1MjuTg==}
     engines: {node: '>= 10'}
     cpu: [ia32]
     os: [win32]
@@ -4890,8 +4894,8 @@ packages:
     dev: false
     optional: true
 
-  /@next/swc-win32-x64-msvc@13.5.3:
-    resolution: {integrity: sha512-2Vz2tYWaLqJvLcWbbTlJ5k9AN6JD7a5CN2pAeIzpbecK8ZF/yobA39cXtv6e+Z8c5UJuVOmaTldEAIxvsIux/Q==}
+  /@next/swc-win32-x64-msvc@14.0.3:
+    resolution: {integrity: sha512-ERhKPSJ1vQrPiwrs15Pjz/rvDHZmkmvbf/BjPN/UCOI++ODftT0GtasDPi0j+y6PPJi5HsXw+dpRaXUaw4vjuQ==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [win32]
@@ -7096,7 +7100,17 @@ packages:
       '@aws-cdk/asset-awscli-v1': 2.2.200
       '@aws-cdk/asset-kubectl-v20': 2.1.2
       '@aws-cdk/asset-node-proxy-agent-v6': 2.0.1
+      '@balena/dockerignore': 1.0.2
+      case: 1.6.3
       constructs: 10.2.69
+      fs-extra: 11.1.1
+      ignore: 5.2.4
+      jsonschema: 1.4.1
+      minimatch: 3.1.2
+      punycode: 2.3.0
+      semver: 7.5.4
+      table: 6.8.1
+      yaml: 1.10.2
     dev: true
     bundledDependencies:
       - '@balena/dockerignore'
@@ -12580,9 +12594,9 @@ packages:
       - babel-plugin-macros
     dev: false
 
-  /next@13.5.3(react-dom@18.2.0)(react@18.2.0):
-    resolution: {integrity: sha512-4Nt4HRLYDW/yRpJ/QR2t1v63UOMS55A38dnWv3UDOWGezuY0ZyFO1ABNbD7mulVzs9qVhgy2+ppjdsANpKP1mg==}
-    engines: {node: '>=16.14.0'}
+  /next@14.0.3(react-dom@18.2.0)(react@18.2.0):
+    resolution: {integrity: sha512-AbYdRNfImBr3XGtvnwOxq8ekVCwbFTv/UJoLwmaX89nk9i051AEY4/HAWzU0YpaTDw8IofUpmuIlvzWF13jxIw==}
+    engines: {node: '>=18.17.0'}
     hasBin: true
     peerDependencies:
       '@opentelemetry/api': ^1.1.0
@@ -12595,26 +12609,25 @@ packages:
       sass:
         optional: true
     dependencies:
-      '@next/env': 13.5.3
+      '@next/env': 14.0.3
       '@swc/helpers': 0.5.2
       busboy: 1.6.0
       caniuse-lite: 1.0.30001525
-      postcss: 8.4.14
+      postcss: 8.4.31
       react: 18.2.0
       react-dom: 18.2.0(react@18.2.0)
       styled-jsx: 5.1.1(@babel/core@7.22.11)(react@18.2.0)
       watchpack: 2.4.0
-      zod: 3.21.4
     optionalDependencies:
-      '@next/swc-darwin-arm64': 13.5.3
-      '@next/swc-darwin-x64': 13.5.3
-      '@next/swc-linux-arm64-gnu': 13.5.3
-      '@next/swc-linux-arm64-musl': 13.5.3
-      '@next/swc-linux-x64-gnu': 13.5.3
-      '@next/swc-linux-x64-musl': 13.5.3
-      '@next/swc-win32-arm64-msvc': 13.5.3
-      '@next/swc-win32-ia32-msvc': 13.5.3
-      '@next/swc-win32-x64-msvc': 13.5.3
+      '@next/swc-darwin-arm64': 14.0.3
+      '@next/swc-darwin-x64': 14.0.3
+      '@next/swc-linux-arm64-gnu': 14.0.3
+      '@next/swc-linux-arm64-musl': 14.0.3
+      '@next/swc-linux-x64-gnu': 14.0.3
+      '@next/swc-linux-x64-musl': 14.0.3
+      '@next/swc-win32-arm64-msvc': 14.0.3
+      '@next/swc-win32-ia32-msvc': 14.0.3
+      '@next/swc-win32-x64-msvc': 14.0.3
     transitivePeerDependencies:
       - '@babel/core'
       - babel-plugin-macros
@@ -13260,6 +13273,15 @@ packages:
       nanoid: 3.3.6
       picocolors: 1.0.0
       source-map-js: 1.0.2
+
+  /postcss@8.4.31:
+    resolution: {integrity: sha512-PS08Iboia9mts/2ygV3eLpY5ghnUcfLV/EXTOW1E2qYxJKGGBUtNjN76FYHnMs36RmARn41bC0AZmn+rR0OVpQ==}
+    engines: {node: ^10 || ^12 || >=14}
+    dependencies:
+      nanoid: 3.3.6
+      picocolors: 1.0.0
+      source-map-js: 1.0.2
+    dev: false
 
   /preact-render-to-string@5.2.6(preact@10.17.1):
     resolution: {integrity: sha512-JyhErpYOvBV1hEPwIxc/fHWXPfnEGdRKxc8gFdAZ7XV4tlzyzG847XAyEZqoDnynP88akM4eaHcSOzNcLWFguw==}

packages/tests-e2e/tests/appRouter/revalidateTag.test.ts
@@ -1,0 +1,56 @@
+import { expect, test } from "@playwright/test";
+
+test("Revalidate tag", async ({ page, request }) => {
+  test.setTimeout(45000);
+  let responsePromise = page.waitForResponse((response) => {
+    return response.status() === 200;
+  });
+  await page.goto("/revalidate-tag");
+  let elLayout = page.getByText("Fetched time:");
+  let time = await elLayout.textContent();
+  let newTime;
+
+  let response = await responsePromise;
+  const nextCacheHeader = response.headers()["x-nextjs-cache"];
+  expect(nextCacheHeader).toMatch(/^(HIT|STALE)$/);
+
+  // Send revalidate tag request
+
+  const result = await request.get("/api/revalidate-tag");
+  expect(result.status()).toEqual(200);
+  const text = await result.text();
+  expect(text).toEqual("ok");
+
+  responsePromise = page.waitForResponse((response) => {
+    return response.status() === 200;
+  });
+  await page.reload();
+  elLayout = page.getByText("Fetched time:");
+  newTime = await elLayout.textContent();
+
+  expect(newTime).not.toEqual(time);
+
+  response = await responsePromise;
+  expect(response.headers()["x-nextjs-cache"]).toEqual("MISS");
+
+  //Check if nested page is also a miss
+  responsePromise = page.waitForResponse((response) => {
+    return response.status() === 200;
+  });
+  await page.goto("/revalidate-tag/nested");
+  elLayout = page.getByText("Fetched time:");
+  newTime = await elLayout.textContent();
+  expect(newTime).not.toEqual(time);
+
+  response = await responsePromise;
+  expect(response.headers()["x-nextjs-cache"]).toEqual("MISS");
+
+  // If we hit the page again, it should be a hit
+  responsePromise = page.waitForResponse((response) => {
+    return response.status() === 200;
+  });
+  await page.goto("/revalidate-tag/nested");
+
+  response = await responsePromise;
+  expect(response.headers()["x-nextjs-cache"]).toEqual("HIT");
+});