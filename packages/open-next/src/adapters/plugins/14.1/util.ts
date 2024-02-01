b9eefca37f1846ac05da5a9aa25a2d421cb77542
Manuel Antunes <57446204+Manuel-Antunes@users.noreply.github.com>
2024-02-01 19:41:47 -0300

    Fix Cache Support for Next@14.1.0 (#356)
    * feat: add cache support for next@14.1.0
    
    * fix: lint files
    
    * chore: apply the proposed changes
    
    * Fix typo
    
    * changeset
    
    ---------
    
    Co-authored-by: conico974 <nicodorseuil@yahoo.fr>

---
+++ packages/open-next/src/adapters/plugins/14.1/util.ts
@@ -0,0 +1,31 @@
+import { NextConfig } from "../../config";
+import { debug } from "../../logger.js";
+
+//#override requestHandler
+// @ts-ignore
+export const requestHandler = new NextServer.default({
+  conf: {
+    ...NextConfig,
+    // Next.js compression should be disabled because of a bug in the bundled
+    // `compression` package — https://github.com/vercel/next.js/issues/11669
+    compress: false,
+    // By default, Next.js uses local disk to store ISR cache. We will use
+    // our own cache handler to store the cache on S3.
+    cacheHandler: `${process.env.LAMBDA_TASK_ROOT}/cache.cjs`,
+    cacheMaxMemorySize: 0,
+    experimental: {
+      ...NextConfig.experimental,
+      // This uses the request.headers.host as the URL
+      // https://github.com/vercel/next.js/blob/canary/packages/next/src/server/next-server.ts#L1749-L1754
+      trustHostHeader: true,
+    },
+  },
+  customServer: false,
+  dev: false,
+  dir: __dirname,
+}).getRequestHandler();
+//#endOverride
+
+//#override requireHooks
+debug("No need to override require hooks with next 13.4.20+");
+//#endOverride
