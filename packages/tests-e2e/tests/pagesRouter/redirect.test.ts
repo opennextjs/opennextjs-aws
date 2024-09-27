94d8fe18a1b0665e8ced356b33d1965faf0b9bd3
conico974 <nicodorseuil@yahoo.fr>
2024-09-27 04:36:10 +0200

    prepare for moving the repo (#525)

--- packages/tests-e2e/tests/pagesRouter/redirect.test.ts
+++ packages/tests-e2e/tests/pagesRouter/redirect.test.ts
@@ -3,8 +3,8 @@ import { expect, test } from "@playwright/test";
 test("Single redirect", async ({ page }) => {
   await page.goto("/next-config-redirect-without-locale-support/");
 
-  await page.waitForURL("https://open-next.js.org/");
-  let el = page.getByRole("heading", { name: "Open source Next.js adapter" });
+  await page.waitForURL("https://opennext.js.org/");
+  let el = page.getByRole("heading", { name: "OpenNext" });
   await expect(el).toBeVisible();
 });
 
