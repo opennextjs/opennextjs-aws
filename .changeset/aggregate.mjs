f2cb7d0d349225f6468c6dfdc6b2125a2b37b232
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2024-10-08 12:37:14 +0200

    fix aggregate

--- .changeset/aggregate.mjs
+++ .changeset/aggregate.mjs
@@ -2,51 +2,49 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { execSync } from "node:child_process";
 
-const THANKLESS_COMMITTERS = ["thdxr", "fwang", "jayair"];
+const THANKLESS_COMMITTERS = ["thdxr", "fwang", "jayair", "conico974"];
 
 const { version } = JSON.parse(
   await fs.readFile("./packages/open-next/package.json"),
 );
 
-const changesets = JSON.parse(await fs.readFile(".changeset/config.json"));
-const packages = changesets.fixed[0];
-
 const changes = new Set();
-for (const pkg of packages) {
-  const changelog = path.join(
-    "packages",
-    pkg.split("/").at(-1),
-    "CHANGELOG.md",
-  );
-  const lines = (await fs.readFile(changelog)).toString().split("\n");
-  let start = false;
-  for (let line of lines) {
-    if (!start) {
-      if (line === `## ${version}`) {
-        start = true;
-        continue;
-      }
-    }
 
-    if (start) {
-      if (line.startsWith("-") || line.startsWith("*")) {
-        if (line.includes("Updated dependencies")) continue;
-        if (line.includes("@serverless-stack/")) continue;
-
-        for (const user of THANKLESS_COMMITTERS) {
-          line = line.replace(
-            `Thanks [@${user}](https://github.com/${user})! `,
-            "",
-          );
-        }
-        changes.add(line);
-        continue;
-      }
-
-      if (line.startsWith("## ")) break;
+// We only need to look for changes in packages/open-next
+const changelog = path.join(
+  "packages",
+  "open-next",
+  "CHANGELOG.md",
+);
+const lines = (await fs.readFile(changelog)).toString().split("\n");
+let start = false;
+for (let line of lines) {
+  if (!start) {
+    if (line === `## ${version}`) {
+      start = true;
+      continue;
     }
   }
+
+  if (start) {
+    if (line.startsWith("-") || line.startsWith("*")) {
+      if (line.includes("Updated dependencies")) continue;
+      if (line.includes("@serverless-stack/")) continue;
+
+      for (const user of THANKLESS_COMMITTERS) {
+        line = line.replace(
+          `Thanks [@${user}](https://github.com/${user})! `,
+          "",
+        );
+      }
+      changes.add(line);
+      continue;
+    }
+
+    if (line.startsWith("## ")) break;
+  }
 }
+
 
 const notes = ["#### Changes", ...changes];
 console.log(notes.join("\n"));
