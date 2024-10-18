b3877f459ae42c39c163e8d9178329ee84a980be
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2024-10-18 22:35:55 +0200

    moved originResolver

--- packages/open-next/src/adapters/middleware.ts
+++ packages/open-next/src/adapters/middleware.ts
@@ -1,69 +1,21 @@
-import { InternalEvent, Origin, OriginResolver } from "types/open-next";
+import { InternalEvent, Origin } from "types/open-next";
 
-import { debug, error } from "../adapters/logger";
+import { debug } from "../adapters/logger";
 import { createGenericHandler } from "../core/createGenericHandler";
 import {
   resolveIncrementalCache,
+  resolveOriginResolver,
   resolveQueue,
   resolveTagCache,
 } from "../core/resolve";
 import routingHandler from "../core/routingHandler";
 
-const resolveOriginResolver = () => {
-  const openNextParams = globalThis.openNextConfig.middleware;
-  if (typeof openNextParams?.originResolver === "function") {
-    return openNextParams.originResolver();
-  }
-
-  return Promise.resolve<OriginResolver>({
-    name: "env",
-    resolve: async (_path: string) => {
-      try {
-        const origin = JSON.parse(
-          process.env.OPEN_NEXT_ORIGIN ?? "{}",
-        ) as Record<string, Origin>;
-        for (const [key, value] of Object.entries(
-          globalThis.openNextConfig.functions ?? {},
-        ).filter(([key]) => key !== "default")) {
-          if (
-            value.patterns.some((pattern) => {
-              // Convert cloudfront pattern to regex
-              return new RegExp(
-                // transform glob pattern to regex
-                "/" +
-                  pattern
-                    .replace(/\*\*/g, "(.*)")
-                    .replace(/\*/g, "([^/]*)")
-                    .replace(/\//g, "\\/")
-                    .replace(/\?/g, "."),
-              ).test(_path);
-            })
-          ) {
-            debug("Using origin", key, value.patterns);
-            return origin[key];
-          }
-        }
-        if (_path.startsWith("/_next/image") && origin["imageOptimizer"]) {
-          debug("Using origin", "imageOptimizer", _path);
-          return origin["imageOptimizer"];
-        }
-        if (origin["default"]) {
-          debug("Using default origin", origin["default"], _path);
-          return origin["default"];
-        }
-        return false as const;
-      } catch (e) {
-        error("Error while resolving origin", e);
-        return false as const;
-      }
-    },
-  });
-};
-
 globalThis.internalFetch = fetch;
 
 const defaultHandler = async (internalEvent: InternalEvent) => {
-  const originResolver = await resolveOriginResolver();
+  const originResolver = await resolveOriginResolver(
+    globalThis.openNextConfig.middleware?.originResolver,
+  );
 
   //#override includeCacheInMiddleware
   globalThis.tagCache = await resolveTagCache(
---
+++ packages/open-next/src/overrides/originResolver/pattern-env.ts
@@ -0,0 +1,50 @@
+import { Origin, OriginResolver } from "types/open-next";
+
+import { debug, error } from "../../adapters/logger";
+
+const envLoader: OriginResolver = {
+  name: "env",
+  resolve: async (_path: string) => {
+    try {
+      const origin = JSON.parse(process.env.OPEN_NEXT_ORIGIN ?? "{}") as Record<
+        string,
+        Origin
+      >;
+      for (const [key, value] of Object.entries(
+        globalThis.openNextConfig.functions ?? {},
+      ).filter(([key]) => key !== "default")) {
+        if (
+          value.patterns.some((pattern) => {
+            // Convert cloudfront pattern to regex
+            return new RegExp(
+              // transform glob pattern to regex
+              "/" +
+                pattern
+                  .replace(/\*\*/g, "(.*)")
+                  .replace(/\*/g, "([^/]*)")
+                  .replace(/\//g, "\\/")
+                  .replace(/\?/g, "."),
+            ).test(_path);
+          })
+        ) {
+          debug("Using origin", key, value.patterns);
+          return origin[key];
+        }
+      }
+      if (_path.startsWith("/_next/image") && origin["imageOptimizer"]) {
+        debug("Using origin", "imageOptimizer", _path);
+        return origin["imageOptimizer"];
+      }
+      if (origin["default"]) {
+        debug("Using default origin", origin["default"], _path);
+        return origin["default"];
+      }
+      return false as const;
+    } catch (e) {
+      error("Error while resolving origin", e);
+      return false as const;
+    }
+  },
+};
+
+export default envLoader;
