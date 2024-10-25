5fed638eec97faef10ad81fc904a8b85759d5e9d
magnus <magnus@dahleide.com>
2024-10-25 11:30:53 +0200

    remove example folder

--- example/components/date.js
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-import LocalizedFormat from "dayjs/plugin/localizedFormat";
-
-dayjs.extend(LocalizedFormat);
-
-export default function Date({ dateString }) {
-  const date = dayjs(dateString);
-  return <time dateTime={dateString}>{date.format("LLLL d, yyyy")}</time>;
-}
