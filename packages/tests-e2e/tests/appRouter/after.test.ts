ecc820b4568455fceabeb7da0e9861b400356db3
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2024-11-11 15:06:28 +0100

    fix lint

--- packages/tests-e2e/tests/appRouter/after.test.ts
+++ packages/tests-e2e/tests/appRouter/after.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("Next after", async ({ request }) => {
   const initialSSG = await request.get("/api/after/ssg");
