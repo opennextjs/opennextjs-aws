b71a91d2a03074599293584dfdf68e3877be47dc
James <james@eli.cx>
2025-01-17 08:32:02 +0000

    add comment

--- packages/open-next/src/utils/regex.ts
+++ packages/open-next/src/utils/regex.ts
@@ -1,3 +1,14 @@
+/**
+ * Constructs a regular expression for a path that supports separators for multiple platforms
+ *  - Uses posix separators (`/`) as the input that should be made cross-platform.
+ *  - Special characters are escaped by default but can be controlled through opts.escape.
+ *
+ * @example
+ * ```ts
+ * getCrossPlatformPathRegex("./middleware.mjs")
+ * getCrossPlatformPathRegex("\\./middleware\\.(mjs|cjs)", { escape: false })
+ * ```
+ */
 export function getCrossPlatformPathRegex(
   regex: string,
   opts: { escape: boolean } = { escape: true },
