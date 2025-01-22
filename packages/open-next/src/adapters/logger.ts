1d4c11600601b32638d9b7771ffb5a755b0826eb
magnus <magnus@dahleide.com>
2025-01-22 21:57:27 +0100

    fix: return when an error is downplayed

--- packages/open-next/src/adapters/logger.ts
+++ packages/open-next/src/adapters/logger.ts
@@ -43,7 +43,7 @@ const isDownplayedErrorLog = (errorLog: AwsSdkClientCommandErrorLog) =>
 export function error(...args: any[]) {
   // we try to catch errors from the aws-sdk client and downplay some of them
   if (args.some((arg) => isDownplayedErrorLog(arg))) {
-    debug(...args);
+    return debug(...args);
   } else if (args.some((arg) => isOpenNextError(arg))) {
     // In case of an internal error, we log it with the appropriate log level
     const error = args.find((arg) => isOpenNextError(arg))!;
