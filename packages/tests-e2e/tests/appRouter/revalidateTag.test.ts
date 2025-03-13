1698112491676a23c24fa7e376d13eaf5262ba2d
James Anderson <james@eli.cx>
2025-03-13 08:30:22 +0000

    Update packages/tests-e2e/tests/appRouter/revalidateTag.test.ts

--- packages/tests-e2e/tests/appRouter/revalidateTag.test.ts
+++ packages/tests-e2e/tests/appRouter/revalidateTag.test.ts
@@ -79,6 +79,7 @@ test("Revalidate path", async ({ page, request }) => {
   elLayout = page.getByText("Date:");
   const initialDate = await elLayout.textContent();
 
+  // Wait so that enough time passes for the data on the page to update when revalidating
   await page.waitForTimeout(2000);
 
   // Send revalidate path request
