37cc7e06eec559b3118c54d2a80928ada2d3e7e2
magnus <magnus@dahleide.com>
2025-03-18 14:17:05 +0100

    add tests for additional routes

--- packages/tests-e2e/tests/appRouter/methods.test.ts
+++ packages/tests-e2e/tests/appRouter/methods.test.ts
@@ -91,3 +91,110 @@ test.describe("all supported methods should work in route handlers", () => {
     expect(headers["special"]).toContain("OpenNext is the best :) :] :> :D");
   });
 });
+
+test("formData should work in POST route handler", async ({ request }) => {
+  const formData = new FormData();
+  formData.append("name", "OpenNext [] () %&#!%$#");
+  formData.append("email", "opennext@opennext.com");
+  const postRes = await request.post("/methods/post/formdata", {
+    form: formData,
+  });
+  expect(postRes.status()).toBe(202);
+  const postData = await postRes.json();
+  expect(postData.message).toBe("ok");
+});
+
+test("revalidate should work in GET route handler", async ({
+  request,
+  page,
+}) => {
+  let time = Date.parse(
+    (await request.get("/methods/get/revalidate").then((res) => res.json()))
+      .time,
+  );
+  let newTime: number;
+  let tempTime = time;
+  do {
+    await page.waitForTimeout(1000);
+    time = tempTime;
+    const newTimeRes = await request.get("/methods/get/revalidate");
+    newTime = Date.parse((await newTimeRes.json()).time);
+    tempTime = newTime;
+  } while (time !== newTime);
+  const midTime = Date.parse(
+    (await request.get("/methods/get/revalidate").then((res) => res.json()))
+      .time,
+  );
+
+  await page.waitForTimeout(1000);
+  // Expect that the time is still stale
+  expect(midTime).toEqual(newTime);
+
+  // Wait 5 + 1 seconds for ISR to regenerate time
+  await page.waitForTimeout(6000);
+  let finalTime = newTime;
+  do {
+    await page.waitForTimeout(2000);
+    finalTime = Date.parse(
+      (await request.get("/methods/get/revalidate").then((res) => res.json()))
+        .time,
+    );
+  } while (newTime === finalTime);
+
+  expect(newTime).not.toEqual(finalTime);
+});
+
+test("should cache a static GET route", async ({ request }) => {
+  const res = await request.get("/methods/get/static");
+  expect(res.headers()["cache-control"]).toBe("s-maxage=31536000,");
+});
+
+test("should be able to set cookies in route handler", async ({ request }) => {
+  const postRes = await request.post("/methods/post/cookies", {
+    form: {
+      username: "hakuna",
+      password: "matata",
+    },
+  });
+  expect(postRes.status()).toBe(202);
+  const postData = await postRes.json();
+  expect(postData.message).toBe("ok");
+  const cookies = postRes.headers()["set-cookie"];
+  expect(cookies).toContain("auth_session=SUPER_SECRET_SESSION_ID_1234");
+});
+
+test("should be able to redirect in route handler", async ({ request }) => {
+  const redirectRes = await request.get("/methods/get/redirect", {
+    // Disable auto-redirect to check initial response
+    maxRedirects: 0,
+  });
+  expect(redirectRes.status()).toBe(307);
+  expect(redirectRes.headers()["location"]).toBe("https://nextjs.org/");
+
+  // Check if the redirect works
+  const followedRes = await request.get("/methods/get/redirect");
+  expect(followedRes.url()).toBe("https://nextjs.org/");
+});
+
+test("dynamic segments should work in route handlers", async ({
+  request,
+}) => {
+  const res = await request.get("/methods/get/dynamic-segments/this-is-a-slug");
+  const data = await res.json();
+  expect(data.slug).toBe("this-is-a-slug");
+});
+
+test("query parameters should work in route handlers", async ({
+  request,
+}) => {
+  const res = await request.get(
+    "/methods/get/query",
+    {
+      params: {
+        query: "OpenNext is awesome!"
+      }
+    }
+  );
+  const data = await res.json();
+  expect(data.query).toBe("OpenNext is awesome!");
+});
