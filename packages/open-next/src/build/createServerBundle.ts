9fd0a27c36d36a38303236dafd26a19a4c690c49
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-03-18 13:51:58 +0100

    patch fetch cache waitUntil

--- packages/open-next/src/build/createServerBundle.ts
+++ packages/open-next/src/build/createServerBundle.ts
@@ -17,6 +17,7 @@ import { generateEdgeBundle } from "./edge/createEdgeBundle.js";
 import * as buildHelper from "./helper.js";
 import { installDependencies } from "./installDeps.js";
 import { type CodePatcher, applyCodePatches } from "./patch/codePatcher.js";
+import { patchFetchCacheSetMissingWaitUntil } from "./patch/patchFetchCacheWaitUntil.js";
 
 interface CodeCustomization {
   // These patches are meant to apply on user and next generated code
---
+++ packages/open-next/src/build/patch/patchFetchCacheWaitUntil.ts
@@ -0,0 +1,42 @@
+import { getCrossPlatformPathRegex } from "utils/regex.js";
+import { createPatchCode } from "./astCodePatcher.js";
+import type { CodePatcher } from "./codePatcher";
+
+export const rule = `
+rule:
+  kind: call_expression
+  pattern: $PROMISE
+  all:
+    - has: { pattern: $_.arrayBuffer().then, stopBy: end }
+    - has: { pattern: "Buffer.from", stopBy: end }
+    - any:
+        - inside:
+            kind: sequence_expression
+            inside:
+                kind: return_statement
+        - inside:
+            kind: expression_statement
+            precedes:
+                kind: return_statement
+    - has: { pattern: $_.FETCH, stopBy: end }
+
+fix: |
+  globalThis.__openNextAls?.getStore()?.pendingPromiseRunner.add($PROMISE)
+`;
+
+export const patchFetchCacheSetMissingWaitUntil: CodePatcher = {
+  name: "patch-fetch-cache-set-missing-wait-until",
+  patches: [
+    {
+      versions: ">=15.0.0",
+      field: {
+        pathFilter: getCrossPlatformPathRegex(
+          String.raw`(server/chunks/.*\.js|.*\.runtime\..*\.js|patch-fetch\.js)$`,
+          { escape: false },
+        ),
+        contentFilter: /arrayBuffer\(\)\s*\.then/,
+        patchCode: createPatchCode(rule),
+      },
+    },
+  ],
+};
