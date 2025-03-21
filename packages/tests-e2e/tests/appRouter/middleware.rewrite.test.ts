f3202c989222eeb7e49c1df9737c6f47454c5c57
magnus <magnus@dahleide.com>
2025-03-21 09:58:16 +0100

    add e2e for external rewrite in middleware in app-router

--- packages/tests-e2e/tests/appRouter/middleware.rewrite.test.ts
+++ packages/tests-e2e/tests/appRouter/middleware.rewrite.test.ts
@@ -1,4 +1,8 @@
 import { expect, test } from "@playwright/test";
+import { validateMd5 } from "../utils";
+
+// md5sum of https://opennext.js.org/share.png
+const OPENNEXT_PNG_MD5 = "405f45cc3397b09717a13ebd6f1e027b";
 
 test("Middleware Rewrite", async ({ page }) => {
   await page.goto("/");
@@ -13,4 +17,14 @@ test("Middleware Rewrite", async ({ page }) => {
   await page.waitForURL("/rewrite");
   el = page.getByText("Rewritten Destination", { exact: true });
   await expect(el).toBeVisible();
+
+  // External rewrite should work in middleware
+  await page.goto("/rewrite-external");
+  page.on("response", async (response) => {
+    expect(response.status()).toBe(200);
+    expect(response.headers()["content-type"]).toBe("image/png");
+    expect(response.headers()["cache-control"]).toBe("max-age=600");
+    const bodyBuffer = await response.body();
+    expect(validateMd5(bodyBuffer, OPENNEXT_PNG_MD5)).toBe(true);
+  });
 });
