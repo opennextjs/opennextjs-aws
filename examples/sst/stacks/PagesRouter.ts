9d1fad896e667778cb94fc319479b15cd025e25e
magnus <magnus@dahleide.com>
2025-03-24 14:26:22 +0100

    add comment

--- examples/sst/stacks/PagesRouter.ts
+++ examples/sst/stacks/PagesRouter.ts
@@ -3,6 +3,11 @@ import { OpenNextCdkReferenceImplementation } from "./OpenNextReferenceImplement
 export function PagesRouter({ stack }) {
   const site = new OpenNextCdkReferenceImplementation(stack, "pagesrouter", {
     path: "../pages-router",
+    /*
+     * We need to set this environment variable to not break other E2E tests that have an empty body. (i.e: /redirect)
+     * https://opennext.js.org/aws/common_issues#empty-body-in-response-when-streaming-in-aws-lambda
+     *
+     */
     environment: {
       OPEN_NEXT_FORCE_NON_EMPTY_RESPONSE: "true",
     },
