4aca20abe3e529f9b8c5317c5198953e9c40e1ef
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-04-04 17:10:02 +0200

    return the user 500 page in case of middleware error

--- examples/pages-router/src/middleware.ts
+++ examples/pages-router/src/middleware.ts
@@ -2,6 +2,9 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  if (request.headers.get("x-throw")) {
+    throw new Error("Middleware error");
+  }
   return NextResponse.next({
     headers: {
       "x-from-middleware": "true",
