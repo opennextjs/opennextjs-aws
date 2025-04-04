f390e0b90bf15c415c7f95fbb173c1e8a26fa575
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-04-04 17:36:00 +0200

    fix lint

--- packages/tests-e2e/tests/pagesRouter/middleware.test.ts
+++ packages/tests-e2e/tests/pagesRouter/middleware.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "playwright/test";
+import { expect, test } from "playwright/test";
 
 test("should return 500 on middleware error", async ({ request }) => {
   const response = await request.get("/", {
