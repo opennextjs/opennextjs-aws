95b3940d2aab5332db7452b83217900564f338d0
conico974 <nicodorseuil@yahoo.fr>
2025-04-12 16:17:34 +0200

    Apply suggestions from code review
    Co-authored-by: Victor Berchet <victor@suumit.com>

--- packages/tests-e2e/tests/appPagesRouter/middleware.test.ts
+++ packages/tests-e2e/tests/appPagesRouter/middleware.test.ts
@@ -4,8 +4,8 @@ test("should return correctly on HEAD request with an empty body", async ({
   request,
 }) => {
   const response = await request.head("/head");
-  const body = await response.text();
   expect(response.status()).toBe(200);
+  const body = await response.text();
   expect(body).toBe("");
   expect(response.headers()["x-from-middleware"]).toBe("true");
 });
@@ -14,7 +14,7 @@ test("should return correctly for directly returning a fetch response", async ({
   request,
 }) => {
   const response = await request.get("/fetch");
-  const body = await response.json();
   expect(response.status()).toBe(200);
+  const body = await response.json();
   expect(body).toEqual({ hello: "world" });
 });
