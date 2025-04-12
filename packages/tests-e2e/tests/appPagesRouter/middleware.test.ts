b85a7c2e78cf52616e37d766f94e278f7d32a3d9
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-04-12 11:54:10 +0200

    lint fix

--- packages/tests-e2e/tests/appPagesRouter/middleware.test.ts
+++ packages/tests-e2e/tests/appPagesRouter/middleware.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("should return correctly on HEAD request with an empty body", async ({
   request,
