989f2b32d8ef3f6fd157daeab038e3375f7a72fe
magnus <magnus@dahleide.com>
2025-05-08 23:49:22 +0200

    chore: make fs-dev imageLoader work in monorepo

--- packages/open-next/src/overrides/imageLoader/fs-dev.ts
+++ packages/open-next/src/overrides/imageLoader/fs-dev.ts
@@ -1,11 +1,14 @@
 import fs from "node:fs";
+import path from "node:path";
+
 import type { ImageLoader } from "types/overrides";
+import { getMonorepoRelativePath } from "utils/normalize-path";
 
 export default {
   name: "fs-dev",
   load: async (url: string) => {
-    const basePath = "../../assets";
-    const body = fs.createReadStream(`${basePath}/${url}`);
+    const imagePath = path.join(getMonorepoRelativePath(), "assets", url);
+    const body = fs.createReadStream(imagePath);
     const contentType = url.endsWith(".png") ? "image/png" : "image/jpeg";
     return {
       body,
