63b7b154dea8079368ae26d3c1a44e166a18129b
magnus <magnus@dahleide.com>
2025-05-24 09:55:25 +0200

    improve e2e

--- examples/app-pages-router/pages/[[...page]].tsx
+++ examples/app-pages-router/pages/[[...page]].tsx
@@ -6,13 +6,18 @@ import type {
 } from "next";
 
 const validRootPages = ["conico974", "kheuzy", "sommeeer"];
+const validLongPaths = ["super/long/path/to/secret/page"];
 
 export async function getStaticPaths(): Promise<GetStaticPathsResult> {
   const rootPaths = validRootPages.map((page) => ({
     params: { page: [page] },
   }));
 
-  const paths = [{ params: { page: [] } }, ...rootPaths];
+  const longPaths = validLongPaths.map((path) => ({
+    params: { page: path.split("/") },
+  }));
+
+  const paths = [{ params: { page: [] } }, ...rootPaths, ...longPaths];
 
   return {
     paths,
@@ -40,6 +45,10 @@ export async function getStaticProps(context: GetStaticPropsContext) {
     };
   }
 
+  const pagePath = page.join("/");
+  if (validLongPaths.includes(pagePath)) {
+    return { props: { subpage: page, pageType: "long-path" } };
+  }
   return { notFound: true };
 }
 
@@ -52,7 +61,8 @@ export default function Page({
   }
   return (
     <div>
-      <h1>{pageType === "home" ? "Homepage" : `Root page: ${subpage}`}</h1>
+      <h1 data-testid="page">{`Page: ${subpage}`}</h1>
+      <p>Page type: {pageType}</p>
       <p>Path: {subpage.join("/")}</p>
     </div>
   );
--- examples/sst/sst.config.ts
+++ examples/sst/sst.config.ts
@@ -1,9 +1,6 @@
 import type { SSTConfig } from "sst";
 
 import { AppPagesRouter } from "./stacks/AppPagesRouter";
-import { AppRouter } from "./stacks/AppRouter";
-import { Experimental } from "./stacks/Experimental";
-import { PagesRouter } from "./stacks/PagesRouter";
 
 export default {
   config(_input) {
@@ -14,9 +11,9 @@ export default {
   },
   stacks(app) {
     app
-      .stack(AppRouter)
-      .stack(PagesRouter)
-      .stack(AppPagesRouter)
-      .stack(Experimental);
+      // .stack(AppRouter)
+      // .stack(PagesRouter)
+      .stack(AppPagesRouter);
+    // .stack(Experimental);
   },
 } satisfies SSTConfig;
