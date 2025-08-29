63a8a41a77281221af70befb5067d648b64dde61
Magnus Dahl Eide <magnus@dahleide.com>
2025-08-29 23:42:14 +0200

    add port constant

--- examples/app-pages-router/proxy.ts
+++ examples/app-pages-router/proxy.ts
@@ -3,6 +3,8 @@ import { spawn } from "node:child_process";
 import express from "express";
 import proxy from "express-http-proxy";
 
+const PORT = 3002;
+
 // Start servers
 spawn("node", [".open-next/server-functions/default/index.mjs"], {
   env: { ...process.env, PORT: "3003" },
@@ -21,7 +23,7 @@ app.use(
   proxy("http://localhost:3004", {
     proxyReqPathResolver: (req) => req.originalUrl,
     proxyReqOptDecorator: (proxyReqOpts) => {
-      proxyReqOpts.headers.host = "localhost:3002";
+      proxyReqOpts.headers.host = `localhost:${PORT}`;
       return proxyReqOpts;
     },
   }),
@@ -36,13 +38,13 @@ app.use(
       // We need to ensure the host header is set correctly else you will run into this error in `/server-actions`
       // Error: Invalid Server Actions request:
       // `x-forwarded-host` header with value `localhost:3003` does not match `origin` header with value `localhost:3002` from a forwarded Server Actions request. Aborting the action.
-      proxyReqOpts.headers.host = "localhost:3002";
+      proxyReqOpts.headers.host = `localhost:${PORT}`;
       delete proxyReqOpts.headers["x-forwarded-host"];
       return proxyReqOpts;
     },
   }),
 );
 
-app.listen(3002, () => {
-  console.log("Proxy running at http://localhost:3002");
+app.listen(PORT, () => {
+  console.log(`Proxy running at http://localhost:${PORT}`);
 });
