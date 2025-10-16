605c4d443575f05950a2a38890a883387c4ebfba
Victor Berchet <victor@suumit.com>
2025-10-16 10:02:17 +0200

    fixup! add support for pnpm

--- packages/open-next/src/build/copyAdapterFiles.ts
+++ packages/open-next/src/build/copyAdapterFiles.ts
@@ -1,38 +1,73 @@
+import fs from "node:fs";
+import path from "node:path";
 import type { NextAdapterOutputs } from "../adapter";
 import type * as buildHelper from "./helper.js";
-import fs, { copyFileSync } from "node:fs";
-import path from "node:path";
 
 export async function copyAdapterFiles(
-    options: buildHelper.BuildOptions,
-    fnName: string,
-    outputs: NextAdapterOutputs) {
-    const filesToCopy = new Map<string, string>();
+  options: buildHelper.BuildOptions,
+  fnName: string,
+  outputs: NextAdapterOutputs,
+) {
+  const filesToCopy = new Map<string, string>();
 
-      //Copying the files from outputs to the output dir
-      for (const [key, value] of Object.entries(outputs)) {
-          if (["pages", "pagesApi", "appPages", "appRoutes"].includes(key)) {
-              for (const route of ( value as any[])) {
-                  const assets = route.assets;
-                  // We need to copy the filepaths to the output dir
-                  const relativeFilePath = path.relative(options.appPath, route.filePath);
-                  console.log('route.filePath', route.filePath, 'relativeFilePath', relativeFilePath);
-                  filesToCopy.set(route.filePath, `${options.outputDir}/server-functions/${fnName}/${relativeFilePath}`);
+  // Copying the files from outputs to the output dir
+  console.log("\n### Copying pages/app files:");
+  for (const [key, value] of Object.entries(outputs)) {
+    if (["pages", "pagesApi", "appPages", "appRoutes"].includes(key)) {
+      for (const route of value as any[]) {
+        const assets = route.assets;
+        // We need to copy the filepaths to the output dir
+        const relativeFilePath = path.relative(options.appPath, route.filePath);
+        console.log(
+          "route.filePath",
+          route.filePath,
+          "relativeFilePath",
+          relativeFilePath,
+        );
+        filesToCopy.set(
+          route.filePath,
+          `${options.outputDir}/server-functions/${fnName}/${relativeFilePath}`,
+        );
 
-                  // console.log('route.assets', assets);
-                  for (const [relative, from] of Object.entries(assets || {})) {
-                      filesToCopy.set(from as string, `${options.outputDir}/server-functions/${fnName}/${relative}`);
-                  }
-                  // copyFileSync(from, `${options.outputDir}/${relative}`);
-              }
-          }
+        for (const [relative, from] of Object.entries(assets || {})) {
+          console.log("route.assets", from, relative);
+          filesToCopy.set(
+            from as string,
+            `${options.outputDir}/server-functions/${fnName}/${relative}`,
+          );
+        }
+        // copyFileSync(from, `${options.outputDir}/${relative}`);
       }
-      for (const [from, to] of filesToCopy) {
-          // console.log(`Copying ${from} to ${to}`);
-          //make sure the directory exists first
-          const dir = path.dirname(to);
-          await fs.promises.mkdir(dir, { recursive: true });
-          copyFileSync(from, to);
-      }
-      return Array.from(filesToCopy.values());
     }
+  }
+
+  console.log("\n### Copying adapter files:");
+  for (const [from, to] of filesToCopy) {
+    console.log(
+      `- ${path.relative(options.appPath, from)} -> ${path.relative(options.appPath, to)}`,
+    );
+
+    //make sure the directory exists first
+    fs.mkdirSync(path.dirname(to), { recursive: true });
+    // For pnpm symlink we need to do that
+    // see https://github.com/vercel/next.js/blob/498f342b3552d6fc6f1566a1cc5acea324ce0dec/packages/next/src/build/utils.ts#L1932
+    let symlink = "";
+    try {
+      symlink = fs.readlinkSync(from);
+    } catch (e) {
+      //Ignore
+    }
+    if (symlink) {
+      try {
+        fs.symlinkSync(symlink, to);
+      } catch (e: any) {
+        if (e.code !== "EEXIST") {
+          throw e;
+        }
+      }
+    } else {
+      fs.copyFileSync(from, to);
+    }
+  }
+  return Array.from(filesToCopy.values());
+}
