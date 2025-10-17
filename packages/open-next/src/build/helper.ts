b0ef5ad1fab01d44c72b06f50f71eeeacd5ce3d1
Victor Berchet <victor@suumit.com>
2025-10-17 11:05:13 +0200

    fixup! temp disable minification

--- packages/open-next/src/build/helper.ts
+++ packages/open-next/src/build/helper.ts
@@ -148,7 +148,8 @@ export async function esbuildAsync(
     format: "esm",
     platform: "node",
     bundle: true,
-    minify,
+    // TODO(vicb): revert to `minify,`
+    minify: false,
     metafile,
     mainFields: ["module", "main"],
     sourcemap: debug ? "inline" : false,
