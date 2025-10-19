771367d9ac328e1544745407dae8ca4fa70c7dcc
Nicolas Dorseuil <nicolas@gitbook.io>
2025-10-19 17:02:46 +0200

    inline chunks for turbopack

--- packages/open-next/src/adapter.ts
+++ packages/open-next/src/adapter.ts
@@ -15,7 +15,7 @@ import { generateOutput } from "./build/generateOutput.js";
 import * as buildHelper from "./build/helper.js";
 import { addDebugFile } from "./debug.js";
 import type { ContentUpdater } from "./plugins/content-updater.js";
-import { inlineRouteHandler } from "./plugins/inlineRouteHandlers.js";
+import { externalChunksPlugin, inlineRouteHandler } from "./plugins/inlineRouteHandlers.js";
 
 export type NextAdapterOutputs = {
   pages: any[];
@@ -133,5 +133,5 @@ function getAdditionalPluginsFactory(
   buildOpts: buildHelper.BuildOptions,
   outputs: NextAdapterOutputs,
 ) {
-  return (updater: ContentUpdater) => [inlineRouteHandler(updater, outputs)];
+  return (updater: ContentUpdater) => [inlineRouteHandler(updater, outputs), externalChunksPlugin(outputs)];
 }
