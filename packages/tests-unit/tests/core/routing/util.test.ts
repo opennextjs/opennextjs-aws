35a3872219255f1584a48910eb4ca341742cb723
Tsubasa Sakai <javascripter@tsukkun.net>
2025-12-02 14:07:58 +0000

    fix linting issues using pnpm lint:fix

--- packages/tests-unit/tests/core/routing/util.test.ts
+++ packages/tests-unit/tests/core/routing/util.test.ts
@@ -89,24 +89,21 @@ describe("isExternal", () => {
   });
 
   it("returns true for absolute https url different host but same host in path", () => {
-    expect(
-      isExternal(
-        "https://absolute.com/local.com",
-        "local.com",
-      ),
-    ).toBe(true);
+    expect(isExternal("https://absolute.com/local.com", "local.com")).toBe(
+      true,
+    );
   });
 
   it("returns false for same host with port", () => {
-    expect(
-      isExternal("https://localhost:3000/path", "localhost:3000"),
-    ).toBe(false);
+    expect(isExternal("https://localhost:3000/path", "localhost:3000")).toBe(
+      false,
+    );
   });
 
   it("returns true for different port on same hostname", () => {
-    expect(
-      isExternal("https://localhost:3001/path", "localhost:3000"),
-    ).toBe(true);
+    expect(isExternal("https://localhost:3001/path", "localhost:3000")).toBe(
+      true,
+    );
   });
 });
 
