4e09d86cebdbc1448fc14a2e926d9ee71ba1a0f8
James <james@eli.cx>
2026-01-18 17:54:29 +0000

    add comment

--- packages/open-next/src/plugins/inline-require-resolve.ts
+++ packages/open-next/src/plugins/inline-require-resolve.ts
@@ -2,6 +2,14 @@ import fs from "node:fs";
 import { createRequire } from "node:module";
 import type { Plugin } from "esbuild";
 
+/**
+ * Inlines calls to `require.resolve` in JavaScript files.
+ *
+ * esbuild does not statically analyse `require.resolve` calls, and the polyfill
+ * does not include an implementation to handle them. This can be problematic
+ * if you attempt to dynamically import a file built by esbuild that unknowingly
+ * contains `require.resolve` calls, as they will throw an error during import.
+ */
 export const inlineRequireResolvePlugin: Plugin = {
   name: "inline-require-resolve",
   setup: (build) => {
