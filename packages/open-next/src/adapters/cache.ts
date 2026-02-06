8e23e6804b08f209d52de77d2a426138a0323eba
magnus <magnus@dahleide.com>
2026-02-07 00:41:28 +0100

    rm log

--- packages/open-next/src/adapters/cache.ts
+++ packages/open-next/src/adapters/cache.ts
@@ -349,7 +349,6 @@ export default class Cache {
       await this.updateTagsOnSet(key, data, ctx);
       debug("Finished setting cache");
     } catch (e) {
-      console.log("ARE WE HERE");
       error("Failed to set cache", e);
     } finally {
       // We need to resolve the promise even if there was an error
