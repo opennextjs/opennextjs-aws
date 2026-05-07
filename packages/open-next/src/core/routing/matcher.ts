36dd2a15d11e82c056a93af163cf79ff2fbe61c7
Nicolas Dorseuil <nicolas@gitbook.io>
2026-05-07 12:10:20 +0200

    fix linting

--- packages/open-next/src/core/routing/matcher.ts
+++ packages/open-next/src/core/routing/matcher.ts
@@ -312,7 +312,7 @@ function handleTrailingSlashRedirect(
 
   if (
     NextConfig.trailingSlash &&
-    !(event.query["__nextDataReq"] === "1") &&
+    !(event.query.__nextDataReq === "1") &&
     !event.rawPath.endsWith("/") &&
     !event.rawPath.match(/[\w-]+\.[\w]+$/g)
   ) {
